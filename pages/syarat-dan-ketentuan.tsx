71b40862868908fca09e082350f30985eccf99aa
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-11-14 12:41:30 +0700

    feat(Syarat&Ketentuan): S&K page UI

---
+++ pages/syarat-dan-ketentuan.tsx
@@ -0,0 +1,157 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const SyaratDanKetentuan: NextPage = () => {
+  return (
+    <div className="space-y-6 text-sm text-primaryText mx-4 lg:mx-80 mt-10 mb-20">
+      <Head>
+        <title>PaDi UMKM</title>
+      </Head>
+      <h1 className="text-3xl text-center font-medium mb-10">
+        Syarat dan Ketentuan PaDi UMKM
+      </h1>
+      <p className="italic">
+        Selamat datang di bisnis.padiumkm.id (untuk selanjutnya disebut sebagai
+        "PaDi UMKM").
+      </p>
+      <div>
+        Syarat & ketentuan ini mengatur mengenai layanan yang diberikan oleh PT
+        Metra-Net selaku anak perusahaan PT Telkom Indonesia (Persero) Tbk
+        (Telkom) yang didelegasikan untuk mengelola {""}
+        <span className="font-bold">PaDi UMKM</span>
+        {""} (untuk selanjutnya disebut sebagai "PaDi UMKM"). Pengguna
+        diwajibkan membaca dengan utuh syarat dan ketentuan ini sebelum
+        mengakses PaDi UMKM lebih lanjut karena terkait dengan hak dan kewajiban
+        Pengguna di bawah hukum yang berlaku.
+      </div>
+      <div>
+        Pada saat mendaftar, memiliki akun dan mengakses PaDi UMKM, maka
+        Pengguna dianggap telah membaca, mengerti, memahami, menyetujui dan
+        terikat secara hukum terhadap semua isi syarat & ketentuan ini. Syarat &
+        ketentuan ini merupakan bentuk kesepakatan yang sah antara Pengguna
+        dengan PaDi UMKM. Jika Pengguna tidak menyetujui salah satu, sebagian,
+        atau seluruh isi syarat & ketentuan, maka Pengguna tidak diperkenankan
+        menggunakan layanan dalam PaDi UMKM.
+      </div>
+      <ol type="A" className="type-a">
+        <li className="font-bold uppercase ml-4">Pendahuluan</li>
+        <ol className="custom-list-type-revert ml-8">
+          <li>
+            <span className="font-bold">PaDi UMKM</span>, yakni platform
+            pencarian Produk yang ditawarkan dan dijual oleh Usaha Mikro Kecil
+            dan Menengah dengan konsep
+          </li>
+          <li>
+            <span className="font-bold">Pengguna</span> adalah UMKM dan/atau
+            Pembeli, merupakan pihak yang dapat mengakses serta menggunakan
+            layanan PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Pembeli</span> adalah Pengguna terdaftar
+            di dalam PaDi UMKM yang akan melakukan permintaan dan pembelian atas
+            Produk yang dijual oleh UMKM di PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">UMKM</span> adalah Pengguna terdaftar
+            yang melakukan tindakan buka toko dan melakukan penawaran Produknya
+            kepada Pembeli melalui PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Produk</span> adalah Produk barang
+            dan/atau jasa yang ditawarkan oleh UMKM untuk dipesan/ dibeli oleh
+            Pembeli melalui PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Deskripsi Produk</span> adalah suatu
+            uraian yang meliputi kata, angka, gambar maupun materi dalam bentuk
+            apapun yang dapat menjelaskan detil Produk secara jelas, lengkap,
+            tepat dan mudah dimengerti oleh Pembeli ketika akan melakukan
+            transaksi pembelian Produk yang sekurang-kurangnya terdiri dari nama
+            dan spesifikasi teknis Produk, Harga Dasar, stok/ kuantiti Produk
+            yang tersedia, layanan purna jual seperti detil tahapan proses retur
+            maupun garansi Produk apabila terdapat kendala dikemudian hari.
+          </li>
+          <li>
+            <span className="font-bold">Transaksi</span> adalah aktivitas jual
+            beli yang dilakukan oleh Pembeli dan UMKM yang difasilitasi melalui
+            platform PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Harga Dasar</span> adalah nilai dalam
+            satuan mata uang Rupiah atas satuan Produk yang dijual oleh UMKM di
+            PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Total Pembayaran</span> adalah nilai
+            akhir Transaksi dalam satuan mata uang Rupiah yang wajib dibayar
+            oleh Pembeli, yang mana nominal tersebut meliputi Harga Dasar Produk
+            dengan dikalikan jumlah Produk yang dibeli, biaya pengiriman Produk
+            maupun biaya lain yang dipersyaratkan, termasuk namun tidak terbatas
+            pada Pajak yang berlaku pada Transaksi tersebut.
+          </li>
+          <li>
+            <span className="font-bold">Purchase Order</span> adalah dokumen
+            pemesanan yang memuat konfirmasi detil pembelian Produk yang dibuat
+            Pembeli kepada UMKM melalui platform PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Invoice</span> adalah dokumen pemesanan
+            yang memuat jumlah nominal dari transaksi, yang terbit secara
+            otomatis melalui platform PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">Berita Acara Serah Terima</span> adalah
+            dokumen yang menunjukkan bahwa pembeli sudah menerima produk dari
+            UMKM atas transaksi di platform PaDi UMKM.
+          </li>
+          <li>
+            <span className="font-bold">
+              Pembayaran Dimuka/Cash Before Delivery
+            </span>{" "}
+            adalah metode yang mengharuskan Pembeli melakukan pembayaran
+            transaksi sesuai dengan nilai Total Pembayaran sebelum Produk
+            dikirimkan.
+          </li>
+          <li>
+            <span className="font-bold">Pembayaran Tempo/Term of Payment</span>{" "}
+            adalah metode yang mengharuskan Pembeli melakukan pembayaran
+            transaksi sesuai dengan nilai Total Pembayaran paling lambat ___ x
+            24 jam (sesuai dengan pemilihan durasi tempo) setelah Produk
+            diterima oleh Pembeli yang dibuktikan dengan terbitnya dokumen
+            Berita Acara Serah Terima dan{" "}
+            <span className="italic">Invoice</span>.
+          </li>
+          <li>
+            <span className="font-bold">Lokasi Pengiriman</span> adalah tempat
+            yang ditunjuk oleh Pembeli sebagai tujuan pengiriman Produk yang
+            tercantum dalam Purchase Order.
+          </li>
+          <li>
+            <span className="font-bold">Rekening Transaksi</span> adalah
+            rekening bersama yang disepakati oleh PaDi UMKM dan Pengguna untuk
+            kelancaran pelaksanaan Transaksi oleh Pengguna.
+          </li>
+          <li>
+            <span className="font-bold">Hari Kalender</span> adalah seluruh hari
+            berdasarkan kalender Masehi, termasuk hari Sabtu, Minggu, dan hari
+            libur nasional Republik Indonesia.
+          </li>
+          <li>
+            <span className="font-bold">Hari Kerja</span> adalah Hari Kalender
+            kecuali hari Sabtu, hari Minggu dan hari libur nasional Republik
+            Indonesia, pada waktu kantor perbankan buka untuk menjalankan
+            kegiatan usahanya dalam melaksanakan transaksi kliring antar bank.
+          </li>
+        </ol>
+      </ol>
+      <ol type="A">
+        <li className="font-bold uppercase">B. Pendaftaran Akun</li>
+        <ol className="custom-list-type-revert ml-8">
+          
+        </ol>
+      </ol>
+    </div>
+  );
+};
+
+export default SyaratDanKetentuan;
