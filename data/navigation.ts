9d10a2c40bf7b9926ab7af9e750234e9fb0e7bd9
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-11-15 10:51:57 +0700

    feat: add navigation data

---
+++ data/navigation.ts
@@ -0,0 +1,23 @@
+type Navigation = {
+  name: string;
+  link: string;
+};
+
+export const navigations: Navigation[] = [
+  {
+    name: "Mitra PaDi UMKM",
+    link: "https://info.padiumkm.id/company",
+  },
+  {
+    name: "Menjadi Penjual",
+    link: "https://info.padiumkm.id/become-seller",
+  },
+  {
+    name: "Info",
+    link: "https://info.padiumkm.id/",
+  },
+  {
+    name: "Pusat Bantuan",
+    link: "https://info.padiumkm.id/question",
+  },
+];
