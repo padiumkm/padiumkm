937fcea24baef71d956028bf03bbe2b9054ad9af
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-12-22 13:58:23 +0700

    feat: checkout

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -94,7 +94,7 @@ const Navbar = () => {
                   >
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                   </svg>
-                  {cart.length > 0 && (
+                  {cart[0].product.item.name !== "" && (
                     <div className="absolute -right-1.5 -bottom-1.5 bg-red-500 w-4 h-4 rounded-full text-xs flex items-center justify-center text-white font-semibold">
                       {cart.length}
                     </div>
